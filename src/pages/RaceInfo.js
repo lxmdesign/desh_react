13a940704ea045e1760058bcc500c0e45ea0c262
tony <2543459951@qq.com>
2017-08-25 11:29:25 +0800

    导入marked

--- src/pages/RaceInfo.js
+++ src/pages/RaceInfo.js
@@ -2,8 +2,8 @@
  * Created by lorne on 2017/8/24.
  */
 import React, {Component} from 'react';
-import ReactMarkdown from 'react-markdown';
-import {BrowserRouter as Router, Route,Link} from "react-router-dom";
+import markdown from 'marked';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import {getRaceInfo} from '../service/RaceDao';
 import '../styles/RaceInfo.css';
 
@@ -34,58 +34,65 @@ export default class RaceInfo extends Component {
         return !0
     }
 
+    desc = (description) => {
+        var des = markdown(description)
+        return {__html:des}
+    }
+
 
     content = () => {
         if (!this.isEmptyObject(this.state.data.race)) {
-            const navs=[
-                {name:'简介',path:"/"},
-                {name:'主赛信息',path:"/"},
-                {name:'边塞信息',path:"/"}
+            const navs = [
+                {name: '简介', path: "/"},
+                {name: '主赛信息', path: "/"},
+                {name: '边塞信息', path: "/"}
             ];
-            const {name, location, status, ticket_status,begin_date,end_date,logo,RaceIntro,MainInformation,SideInformation
-                ,mainMark,description,Blind,Ranks} = this.state.data.race;
-            return(
-            <div className='content'>
-                {/*Head*/}
-                <div className="mainNav">
-                    <div className="ul-1">
-                        <div className="title">{name}</div>
-                        <img src={logo}/>
-                        <ul className="ul-1-2">
-                            <li>{begin_date}—{end_date}</li>
-                            <li>{location}</li>
-                            <li className="li-4"><span>{status}</span><span>{ticket_status}</span></li>
+            const {
+                name, location, status, ticket_status, begin_date, end_date, logo, RaceIntro, MainInformation, SideInformation
+                , mainMark, description, Blind, Ranks
+            } = this.state.data.race;
+            return (
+                <div className='content'>
+                    {/*Head*/}
+                    <div className="mainNav">
+                        <div className="ul-1">
+                            <div className="title">{name}</div>
+                            <img src={logo}/>
+                            <ul className="ul-1-2">
+                                <li>{begin_date}—{end_date}</li>
+                                <li>{location}</li>
+                                <li className="li-4"><span>{status}</span><span>{ticket_status}</span></li>
+                            </ul>
+                        </div>
+                        <ul className="ul-2" style={{margin: 0}}>
+                            {
+                                navs.map((value, key) =>
+                                    (<li key={key}>
+                                        <Link className="gameActive" to={value.path}>
+                                            {value.name}
+                                        </Link>
+                                    </li>))
+                            }
+                            {/*<li onClick="changeClass(0)" >简介</li>*/}
+                            {/*<li onClick='changeClass(1)' >主赛信息</li>*/}
+                            {/*<li onClick='changeClass(2)' >边塞信息</li>*/}
                         </ul>
                     </div>
-                    <ul className="ul-2" style={{margin:0}} >
-                        {
-                            navs.map((value, key) =>
-                                (<li key={key}>
-                                    <Link className="gameActive" activeClassName="active" to={value.path} >
-                                        {value.name}
-                                    </Link>
-                                </li>))
-                        }
-                      {/*<li onClick="changeClass(0)" >简介</li>*/}
-                      {/*<li onClick='changeClass(1)' >主赛信息</li>*/}
-                      {/*<li onClick='changeClass(2)' >边塞信息</li>*/}
-                    </ul>
-                </div>
-                {/*<div className="introduceGame" >{mainMark}</div>*/}
-                <ReactMarkdown source={description} />
-                {/**/}
-                {/*<div className="mainGame">*/}
-                    {/*<ul className="gameList">*/}
-                        {/*<li onClick='gameClass(0)' >{description}</li>*/}
-                        {/*<li onClick='gameClass(1)' >{Blind} </li>*/}
-                        {/*<li onClick='gameClass(2)' >{Ranks}</li>*/}
-                    {/*</ul>*/}
-                {/*</div>*/}
-                {/*info*/}
-                <div>
 
+                    <div class="introduceGame"  dangerouslySetInnerHTML={this.desc(description)}></div>
+                    {/**/}
+                    {/*<div className="mainGame">*/}
+                    {/*<ul className="gameList">*/}
+                    {/*<li onClick='gameClass(0)' >{description}</li>*/}
+                    {/*<li onClick='gameClass(1)' >{Blind} </li>*/}
+                    {/*<li onClick='gameClass(2)' >{Ranks}</li>*/}
+                    {/*</ul>*/}
+                    {/*</div>*/}
+                    {/*info*/}
+                    <div in>
+
+                    </div>
                 </div>
-            </div>
 
             );
         }
