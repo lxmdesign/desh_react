e2b5411a6c45622560654c973e3533f894817009
lorne <2270333671@qq.com>
2017-08-26 17:53:28 +0800

    牌手赛事

---
+++ src/styles/Colors.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lorne on 2017/8/26.
+ */
+const colors = {
+    bg_ec:'#ECECEE',
+    _F4E:'#F4E3A1',
+    _DF1:'#DF1D0F',
+    _161: '#161718',
+    _333:'#333333',
+    _666: '#666666',
+    _999: '#999999',
+    _CCC: '#CCCCCC',
+    _ECE: '#ECECEE',
+    _000: '#000000'
+};
+
+export default colors;
