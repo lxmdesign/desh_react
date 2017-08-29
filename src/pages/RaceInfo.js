0845881aad7882ad29277a1a678ab1c30f49effb
Millie <millie@deshpro.com>
2017-08-29 16:16:22 +0800

    赛程表

--- src/pages/RaceInfo.js
+++ src/pages/RaceInfo.js
@@ -16,8 +16,11 @@ export default class RaceInfo extends Component {
     state = {
         dataStr: '',
         data: {},
+        schedules:[],
+        blinds:[],
         menu: 0,
-        selectInfo:0
+        selectInfo:0,
+        selectInfo_menu:0
     };
 
     componentDidMount() {
@@ -47,6 +50,7 @@ export default class RaceInfo extends Component {
         var des = markdown(description)
         return {__html: des}
     }
+
     //click事件
 
 
@@ -73,34 +77,36 @@ export default class RaceInfo extends Component {
                         </div>
 
 
-                        <div className="menu">
-                            <div className="menu1" onClick={() => {
-                                this.setState({
-                                    menu: 0
-                                })
-                            }}>
-                                <span className='txtMenu imgMe'>简介</span>
-                                {/*<img src={imgMenu} className="imgMe"/>*/}
-                            </div>
-                            <div className="menu1"
-                                 onClick={() => {
-                                     this.setState({
-                                         menu: 1
-                                     })
-                                 }}>
-                                <span className='txtMenu imgMe'>主赛信息</span>
+                        <div className="menu-fixed">
+                            <div className="menu">
+                                <div className="menu1" onClick={() => {
+                                    this.setState({
+                                        menu: 0
+                                    })
+                                }}>
+                                    <span className='txtMenu imgMe'>简介</span>
+                                    {/*<img src={imgMenu} className="imgMe"/>*/}
+                                </div>
+                                <div className="menu1"
+                                     onClick={() => {
+                                         this.setState({
+                                             menu: 1
+                                         })
+                                     }}>
+                                    <span className='txtMenu imgMe'>主赛信息</span>
+
+                                </div>
+                                <div className="menu1"
+                                     onClick={() => {
+                                         this.setState({
+                                             menu: 2
+                                         })
+                                     }}>
+                                    <span className='txtMenu'>边塞信息</span>
+
+                                </div>
 
                             </div>
-                            <div className="menu1"
-                                 onClick={() => {
-                                     this.setState({
-                                         menu: 2
-                                     })
-                                 }}>
-                                <span className='txtMenu'>边塞信息</span>
-
-                            </div>
-
                         </div>
 
                     </div>
@@ -112,6 +118,7 @@ export default class RaceInfo extends Component {
             );
         }
     }
+    //导航信息选择显示页面
     selectMenu = () => {
         const {
             name, location, status, ticket_status, begin_date, end_date, logo,
@@ -122,10 +129,41 @@ export default class RaceInfo extends Component {
             case 0:
                 return this.introView(description);
             case 1:
-                return this.infoView();
+                return this.mainInfoView();
             case 2:
-                return this.infoView();
+                return this.frontierInfoView();
         }
+    }
+
+    //主赛信息选择显示页面
+    select_mainInfoMenu = () => {
+        switch (this.state.selectInfo_menu) {
+            case 0:
+                return this.scheduleView();
+            case 1:
+                return this.blindStructureView();
+        }
+    }
+
+    //赛程格式化
+    scheduleMessage = (schedule) => {
+        if (schedule.indexOf('|') == -1) {
+            return this.scheduleMessageOne(schedule);
+        } else {
+
+            var sch = schedule.split('|')
+
+            return this.scheduleMessageTwo(sch[0],sch[1]);
+        }
+        //
+        // console.log('sch:'+sch)
+        // return sch;
+    }
+    scheduleMessageOne=(schedule)=>{
+        return <span>{schedule}</span>
+    }
+    scheduleMessageTwo = (schedule1,schedule2)=>{
+        return <span>{schedule1}<br/>{schedule2}</span>
     }
 
     introView = (description) => {
@@ -133,14 +171,15 @@ export default class RaceInfo extends Component {
         return <div className="introduceGame" dangerouslySetInnerHTML={this.desc(description)}></div>;
     };
 
-    infoView = () => {
+    mainInfoView = () => {
 
-        const {selectInfo} = this.state;
+        const {selectInfo,selectInfo_menu} = this.state;
         return <div className="infoView">
             <div className="infoView-nav">
                 <div className={selectInfo === 0?'btn2':'btn1'} onClick={()=>{
                     this.setState({
-                        selectInfo:0
+                        selectInfo:0,
+                        selectInfo_menu:0
                     })
                 }}>
                     <span>赛程表</span>
@@ -148,14 +187,54 @@ export default class RaceInfo extends Component {
                 <div className="clo_line" />
                 <div className={selectInfo === 1?'btn2':'btn1'} onClick={()=>{
                     this.setState({
-                        selectInfo:1
+                        selectInfo:1,
+                        selectInfo_menu:1
                     })
                 }}>
                     <div>盲注结构</div>
                 </div>
             </div>
+
+            {this.select_mainInfoMenu()}
         </div>
     };
+
+    frontierInfoView=() =>{
+
+    };
+
+    scheduleView=()=>{
+        const {
+            schedules
+        } = this.state.data;
+
+        return <div className="schedule">
+            <table>
+                <thead className="schedule-nav">
+                    <span>赛程</span>
+                    <span>日期</span>
+                    <span>开始时间</span>
+                </thead>
+                <tbody>
+                {schedules.map(schedule =>{
+
+                    console.log('list',schedule)
+                    return <tr>
+                        <td>
+                            {this.scheduleMessage(schedule.schedule)}
+                            <span><Time value={schedule.begin_time} format="MM-DD" /></span>
+                            <span><Time value={schedule.begin_time} format="hh:mm" /></span>
+                        </td>
+                    </tr>
+                })}
+                </tbody>
+            </table>
+        </div>
+    }
+
+    blindStructureView=()=>{
+
+    }
 
 
     render() {
