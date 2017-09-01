8cb94c4572994c8d58235052f5f023cf1e2c8cb5
Millie <millie@deshpro.com>
2017-09-01 14:33:35 +0800

    全部完成的代码

---
+++ src/pages/PayFail.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export default class PayFail extends Component {
+    render(){
+        return(
+            <p>
+                pay-fail works!
+            </p>
+        )
+    }
+}
---
+++ src/pages/PaySuccess.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../styles/Pay.css'
+
+export default class PaySuccess extends Component {
+    render(){
+        return(
+            <p>
+                pay-success works!
+            </p>
+        )
+    }
+}
