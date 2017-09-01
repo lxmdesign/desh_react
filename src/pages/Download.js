bc72ecdfc756d1987943d8a00115c94026008b21
Millie <millie@deshpro.com>
2017-09-01 18:36:00 +0800

    分享页面修改

--- src/pages/Download.js
+++ src/pages/Download.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 
-import {getGameInfo, getRankInfo, setLang} from '../service/RaceDao';
 import '../styles/Download.css';
-import Time from 'react-time-format';
 
 export default class Download extends Component {
     render(){
